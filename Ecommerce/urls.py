2799c327e4f713d027e550cfb9b4bd9f9d23a347
Harsha Vardhan S M <Smharshavardhan08@gmail.com>
2025-10-24 23:06:38 +0530

    Clean up and simplify URL patterns in urls.py

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -1,34 +1,11 @@
-"""Ecommerce URL Configuration
+"""Ecommerce URL Configuration"""
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-app_name = 'admin'
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin-site"),
-    path('healthkart/', include('shopee.urls'))
+    path('', include('shopee.urls')),  # 👈 Main homepage connects to your app
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('shopee.urls')),  # 👈 this line connects your app
-]
-
