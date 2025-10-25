d43b22aa1c794a05efdea9cbc79d1e8758e59e1d
Harsha Vardhan S M <Smharshavardhan08@gmail.com>
2025-10-25 12:28:55 +0530

    Update settings.py

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -77,9 +77,10 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db_local.sqlite3',
     }
 }
+
 
 
 # Password validation
