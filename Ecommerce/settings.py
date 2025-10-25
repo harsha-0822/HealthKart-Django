f9efda130cbc294551580df42e8e8426cb266ee0
Harsha Vardhan S M <Smharshavardhan08@gmail.com>
2025-10-25 13:27:31 +0530

    Use os.path.join for database name path

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -74,10 +74,12 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db_local.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db_local.sqlite3'),
     }
 }
 
